a833e2a8d8060f66eef2192873c26cc540cd2c37
Nigel Kukard <nkukard@lbsd.net>
2020-04-16 20:45:32 +0000

    Initial commit

---
+++ rt-init.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+
+set -e
+
+
+function wait_for_db() {
+	echo "INFO: Waiting for database"
+	while [ ! "$(mysqladmin ping --silent --connect_timeout=3)" ]; do
+		echo "INFO: Waiting for database..."
+		sleep 1
+	done
+	echo "INFO: Database server is up"
+}
+
+
+# Setup database credentials
+cat <<EOF > /root/.my.cnf
+[mysql]
+user=$MYSQL_USER
+password=$MYSQL_PASSWORD
+[mysqladmin]
+user=$MYSQL_USER
+password=$MYSQL_PASSWORD
+EOF
+
+
+# Create new configuration file
+cat <<EOF > /opt/rt4/etc/RT_SiteConfig.d/00-default.pm
+use utf8;
+
+Set(\$DatabaseType, "mysql");
+Set(\$DatabaseRTHost, "localhost");
+Set(\$DatabaseUser, "$MYSQL_USER");
+Set(\$DatabasePassword, "$MYSQL_PASSWORD");
+Set(\$DatabaseName, "$MYSQL_DATABASE");
+
+Set(\$ExternalStorageCutoffSize, 10_000);
+Set(%ExternalStorage,
+	Type => 'Disk',
+	Path => '/opt/rt4/var/attachments',
+);
+
+require '/opt/rt/RT_SiteConfig.pm';
+
+1;
+EOF
+
+
+# Link in our own config file...
+if [ -e /opt/rt/RT_SiteConfig.pm ]; then
+	ln -s /opt/rt/RT_SiteConfig.pm /opt/rt4/etc/RT_SiteConfig.d/50-custom.pm
+fi
+
+# Make sure we have an attachments directory setup
+if [ ! -d /opt/rt/var/attachments ]; then
+	mkdir -p /opt/rt/var/attachments
+fi
+# Link attachments directory in
+chown rt:www-data /opt/rt/var/attachments
+chmod 750 /opt/rt/var/attachments
+ln -s /opt/rt/var/attachments /opt/rt4/var/
+
+# Make sure we have a shredder directory setup
+if [ ! -d /opt/rt/var/data/RT-Shredder ]; then
+	mkdir -p /opt/rt/var/data/RT-Shredder
+fi
+# Check perms
+chown rt:www-data /opt/rt/var/data/RT-Shredder
+chmod 750 /opt/rt/var/data/RT-Shredder
+# Make sure the dir exists
+if [ ! -d /opt/rt4/var/data ]; then
+	mkdir -p /opt/rt4/var/data
+fi
+# Link it in
+ln -s /opt/rt/var/data/RT-Shredder /opt/rt4/var/data/
+
+# Reset permissions on local/html directory which could be mapped in
+chown rt:www-data /opt/rt4/local/html
+find /opt/rt4/local/html -type d -print0 | xargs -0 -r chmod 0755
+find /opt/rt4/local/html -type f -print0 | xargs -0 -r chmod 0644
+
+# Start MySQL for possible DB changes
+/usr/bin/mysqld --user=mysql --console &
+wait_for_db
+
+# Check if we need to initialize the database
+if [ ! -e /opt/rt/.RT_VERSION ]; then
+	echo "NOTICE: Initialize RT database"
+	cd /opt/rt
+
+	/usr/bin/perl /opt/rt4/sbin/rt-setup-database --action init --skip-create
+
+	echo "$RT_VERSION" > /opt/rt/.RT_VERSION
+	echo "NOTICE: Done initialize RT database"
+fi
+
+if [ ! -e /opt/rt/.RT_EXTENSION_REPEATTICKET ]; then
+	echo "NOTICE: Initialize RT::Extension::RepeatTicket"
+	cd /opt/rt
+	# Repeat ticket
+	/usr/bin/perl /opt/rt4/sbin/rt-setup-database --action insert --skip-create \
+			--datadir "/opt/rt4/local/plugins/RT-Extension-RepeatTicket/etc" \
+			--datafile "/opt/rt4/local/plugins/RT-Extension-RepeatTicket/etc/initialdata" \
+			--package "q[RT::Extension::RepeatTicket]" --ext-version "q[${RT_EXTENSION_REPEATTICKET}]"
+	echo "$RT_EXTENSION_REPEATTICKET" > /opt/rt/.RT_EXTENSION_REPEATTICKET
+	echo "NOTICE: Done initialize RT::Extension::RepeatTicket"
+fi
+
+if [ ! -e /opt/rt/.RT_EXTENSION_RESETPASSWORD ]; then
+	echo "NOTICE: Initialize RT::Extension::ResetPassword"
+	cd /opt/rt
+	# Reset password
+	/usr/bin/perl /opt/rt4/sbin/rt-setup-database --action insert --skip-create \
+			--datadir "/opt/rt4/local/plugins/RT-Extension-ResetPassword/etc" \
+			--datafile "/opt/rt4/local/plugins/RT-Extension-ResetPassword/etc/initialdata" \
+			--package "q[RT::Extension::ResetPassword]" --ext-version "q[${RT_EXTENSION_RESETPASSWORD}]"
+	echo "$RT_EXTENSION_RESETPASSWORD" > /opt/rt/.RT_EXTENSION_RESETPASSWORD
+	echo "NOTICE: Done initialize RT::Extension::ResetPassword"
+fi
+
+# Stop MySQL
+pkill mysqld
+wait
+
+# Create nginx cache directory
+mkdir /var/lib/nginx/cache
+chown nginx:root /var/lib/nginx/cache
+chmod 700 /var/lib/nginx/cache
+
+# Setup crontab if we have LDAP configuration
+if grep -q -E '^\s*Set\(\s*\$LDAPUpdateUsers' /opt/rt4/etc/RT_SiteConfig.d/*; then
+	cat <<EOF | crontab -u rt -
+@reboot /usr/local/sbin/rt-ldap-importer --verbose
+*/15 * * * * /usr/local/sbin/rt-ldap-importer
+EOF
+fi
